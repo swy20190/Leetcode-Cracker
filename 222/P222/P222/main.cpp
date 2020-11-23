ca2c94c8c96c5b9294d556ea7a146bdae2d66521
swy20190 <midnightsun114514@gmail.com>
2020-11-23 09:37:31 +0800

    P222 recursive

---
+++ 222/P222/P222/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+ 
+class Solution {
+public:
+	int countNodes(TreeNode* root) {
+		if (root == nullptr) {
+			return 0;
+		}
+		return 1 + countNodes(root->left) + countNodes(root->right);
+	}
+};
