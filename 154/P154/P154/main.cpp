0b20a5abd83d49d32b00c89134c5c753cab7c249
swy20190 <midnightsun114514@gmail.com>
2020-11-24 11:32:43 +0800

    P154 Hape

---
+++ 154/P154/P154/main.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int findMin(vector<int>& nums) {
+		int len = nums.size();
+		if (len == 1) {
+			return nums[0];
+		}
+		int first = nums[0];
+		int second = INT_MAX;
+		int head = 0;
+		int tail = 1;
+		for (int i = 0; i < len - 1; i++) {
+			if (nums[head] > nums[tail]) {
+				second = nums[tail];
+				break;
+			}
+			head++;
+			tail++;
+		}
+		return min(first, second);
+	}
+};
