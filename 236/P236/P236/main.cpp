7656b3bfc66426a3f82dfcd93def10461ad884f6
swy20190 <midnightsun114514@gmail.com>
2020-11-25 10:49:55 +0800

    P236 recursive

---
+++ 236/P236/P236/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+ 
+class Solution {
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (root == nullptr || p == root || q == root) {
+			return root;
+		}
+		TreeNode* left = lowestCommonAncestor(root->left, p, q);
+		TreeNode* right = lowestCommonAncestor(root->right, p, q);
+		if (left != nullptr && right != nullptr) {
+			return root;
+		}
+		else if (left == nullptr) {
+			return right;
+		}
+		else if (right == nullptr) {
+			return left;
+		}
+		return nullptr;
+	}
+};
