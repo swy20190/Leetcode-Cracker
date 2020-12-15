d0e9cf7e7a3987742c2f576c9220cacf76c4aa68
swy20190 <midnightsun114514@gmail.com>
2020-12-15 17:27:41 +0800

    P231 注意0不是2的幂

---
+++ 231/P231/P231/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isPowerOfTwo(int n) {
+		if (n <= 0) {
+			return false;
+		}
+		if ((n&n - 1) == 0) {
+			return true;
+		}
+		return false;
+	}
+};
