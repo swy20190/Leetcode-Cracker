220041caf997aecdacf9a10bafdabd67871a513a
swy20190 <midnightsun114514@gmail.com>
2021-01-05 19:59:22 +0800

    P535 不讲武德X2

---
+++ 535/P535/P535/main.cpp
@@ -0,0 +1,17 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+
+	// Encodes a URL to a shortened URL.
+	string encode(string longUrl) {
+		return longUrl;
+	}
+
+	// Decodes a shortened URL to its original URL.
+	string decode(string shortUrl) {
+		return shortUrl;
+	}
+};
