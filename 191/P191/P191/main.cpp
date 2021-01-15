26c9318cd7af0ea49d720ad143b11a2a67bffd33
swy20190 <midnightsun114514@gmail.com>
2021-01-15 18:05:01 +0800

    P191 CSAPP

---
+++ 191/P191/P191/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	int hammingWeight(uint32_t n) {
+		int sum = 0;
+		while (n != 0) {
+			sum += n & 1;
+			n = n >> 1;
+		}
+		return sum;
+	}
+};
