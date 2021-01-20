4ef78579c7135e8ffade67f0a4df4d37ff3ee6f5
swy20190 <midnightsun114514@gmail.com>
2021-01-20 16:24:35 +0800

    P628 Hape

---
+++ 628/P628/P628/main.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int maximumProduct(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+		int len = nums.size();
+		return max(nums[0] * nums[1] * nums[len - 1], nums[len - 3] * nums[len - 2] * nums[len - 1]);
+	}
+};
