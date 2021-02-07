e854ff0a96e01a54561509ea40554a1c3a00b056
swy20190 <midnightsun114514@gmail.com>
2021-02-07 17:54:45 +0800

    P455 geedy

---
+++ 455/P455/P455/main.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int findContentChildren(vector<int>& g, vector<int>& s) {
+		sort(g.begin(), g.end());
+		sort(s.begin(), s.end());
+		int index = s.size() - 1; // 饼干数组的下表
+		int result = 0;
+		for (int i = g.size() - 1; i >= 0; i--) {
+			if (index >= 0 && s[index] >= g[i]) {
+				result++;
+				index--;
+			}
+		}
+		return result;
+	}
+};
