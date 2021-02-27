f38b4ca71f19e2e5847e17d18c764e9184523e5c
swy20190 <midnightsun114514@gmail.com>
2021-02-27 16:04:29 +0800

    P395 divide and conquer

---
+++ 395/P395/P395/main.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int longestSubstring(string s, int k) {
+		if (k <= 0) {
+			return s.size();
+		}
+		if (s.empty() || s.size() < k) {
+			return 0;
+		}
+
+		int hash[128] = { 0 };
+		for (char c : s) {
+			hash[c]++;
+		}
+		int i = 0;
+		while(i < s.size() && hash[s[i]] >= k) {
+			i++;
+		}
+		if (i == s.size()) {
+			return s.size();
+		}
+		int left = longestSubstring(s.substr(0, i), k);
+		while (i < s.size() && hash[s[i]] < k) {
+			i++;
+		}
+
+		int right = longestSubstring(s.substr(i), k);
+
+		return max(left, right);
+	}
+};
