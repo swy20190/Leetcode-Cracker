d32806762aa891acf01636621bcc7921655febbc
swy20190 <midnightsun114514@gmail.com>
2021-07-04 14:58:00 +0800

    P998 递归

---
+++ 998/P998/P998/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
+class Solution {
+public:
+	TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
+		if (root == NULL) {
+			return new TreeNode(val);
+		}
+		if (root->val < val) {
+			root = new TreeNode(val, root, NULL);
+		}
+		else {
+			root->right = insertIntoMaxTree(root->right, val);
+		}
+		return root;
+	}
+};
