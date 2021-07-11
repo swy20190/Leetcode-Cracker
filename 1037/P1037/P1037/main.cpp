ef8d876c11c8784b544b0e72fe651a0ffaf16113
swy20190 <midnightsun114514@gmail.com>
2021-07-11 18:06:54 +0800

    P1037 数学题

---
+++ 1037/P1037/P1037/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isBoomerang(vector<vector<int>>& points) {
+		if (points.size() != 3) {
+			return false;
+		}
+		return points[0][0] * points[1][1] - points[1][0] * points[0][1]
+			+ points[1][0] * points[2][1] - points[2][0] * points[1][1]
+			+ points[2][0] * points[0][1] - points[0][0] * points[2][1] != 0;
+	}
+};
