c98851bd63e1de24c3ce7fa790f9f597d1a41e6b
swy20190 <midnightsun114514@gmail.com>
2021-07-31 10:21:28 +0800

    P680 easy

---
+++ 680/P680/P680/main.cpp
@@ -0,0 +1,29 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	bool validPalindrome(string s) {
+		int l = 0;
+		int r = s.size() - 1;
+		while (l < r) {
+			if (s[l] != s[r]) {
+				return isPal(s, l + 1, r) || isPal(s, l, r - 1);
+			}
+			l++;
+			r--;
+		}
+		return true;
+	}
+	bool isPal(string s, int l, int r) {
+		while (l < r) {
+			if (s[l] != s[r]) {
+				return false;
+			}
+			l++;
+			r--;
+		}
+		return true;
+	}
+};
